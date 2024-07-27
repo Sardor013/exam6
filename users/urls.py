6ea6f1cb0433aba0226e57bd9b5f3233a1950cf9
Sardor <sardoromonqulov8@gmail.com>
2024-07-28 02:29:30 +0500

    'generic'

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import register_view, login_view, logout_view
+from .views import RegisterView, CustomLoginView, logout_view
 
 urlpatterns = [
-    path("register/", register_view, name="register"),
-    path("login/", login_view, name="login"),
-    path('logout/', logout_view, name="logout"),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,42 +1,30 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('home')
 
-def register_view(request):
-    form = UserCreationForm()
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')
-        else:
-            return redirect('register')
-    else:
-        context = {
-            "form": form
-        }
-        return render(request, 'users/register.html', context)
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = form.save()
+        login(self.request, user)
+        return response
 
+class CustomLoginView(LoginView):
+    template_name = 'users/login.html'
+    redirect_authenticated_user = True
 
-def login_view(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request.POST)
-        user = authenticate(username=request.POST['username'], password=request.POST['password'])
-        if user:
-            login(request, user)
-            return redirect("home")
-        else:
-            return redirect('login')
-    else:
-        form = AuthenticationForm()
-        context = {
-            "form": form
-        }
-        return render(request, 'users/login.html', context)
+    def get_success_url(self):
+        return reverse_lazy('home')
 
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('home')
 
 def logout_view(request):
     logout(request)
-    return redirect('home')
+    return HttpResponseRedirect(reverse_lazy('home'))
